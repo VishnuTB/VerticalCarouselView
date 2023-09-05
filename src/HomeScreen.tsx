e0c74c1871a4deedd285b85d5c81a7d187b813bb
Vishnu T B <vishnutb123@gmail.com>
2023-09-05 18:27:08 +0530

    changes

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   Animated,
-  Button,
-  FlatList,
   Image,
   StyleProp,
   StyleSheet,
@@ -11,13 +9,6 @@ import {
   View,
   ViewStyle
 } from "react-native";
-import {
-  Directions,
-  FlingGestureHandler,
-  GestureEvent,
-  HandlerStateChangeEvent,
-  State
-} from 'react-native-gesture-handler';
 import { CardItemType } from "./types/CardItemType";
 import { DATA } from "./utils/data";
 import { windowWidth } from "./utils/utils";
@@ -29,37 +20,18 @@ const VISIBLE_ITEMS: number = 3;
 
 const HomeScreen: React.FC = () => {
   const [data, setData] = useState<Array<CardItemType>>(DATA);
-  const scrollYIndex = React.useRef(new Animated.Value(0)).current
+  const scrollX = React.useRef(new Animated.Value(0)).current
   const scrollYAnimtaed = React.useRef(new Animated.Value(0)).current
   const [enableAnimation, setEnableAnimation] = useState<boolean>(false)
   const [currentIndex, setCurrentIndex] = useState<number>(0)
 
-  useEffect(() => {
-    Animated.spring(scrollYAnimtaed, {
-      toValue: scrollYIndex,
-      useNativeDriver: true
-    }).start()
-
-    // Animated.timing(scrollYAnimtaed, {
-    //   toValue: scrollYIndex,
-    //   duration: 300,
-    //   useNativeDriver: true,
-    //   easing: Easing.linear
-    // }).start();
-
-    // setInterval(() => {
-    //   const newIndex: number = Math.floor(Math.random() * data.length)
-    //   console.log('newIndex : ', newIndex)
-    //   scrollYIndex.setValue(newIndex)
-    // }, 2000)
-  })
 
 
   const renderItem = ({ item, index }: { item: CardItemType, index: number }) => {
     const inputRange = [index - 3, index, index + 1];
     const translateY = scrollYAnimtaed.interpolate({
       inputRange,
-      outputRange: [-100, 0, 50]
+      outputRange: [300, 0, 50]
     })
     const scale = scrollYAnimtaed.interpolate({
       inputRange,
@@ -72,20 +44,17 @@ const HomeScreen: React.FC = () => {
 
     return (
       <Animated.View key={index} style={{
-        position: 'absolute',
-        top: -ITEM_WIDTH / 4,
+        // position: 'absolute',
+        // width: '100%',
         transform: [{
           translateY
-        }, {
-          scale
-        }],
-        opacity: opacity
+        }, { scale }],
+        opacity: opacity,
       }}>
         <TouchableOpacity style={styles.cardContainer}
-          activeOpacity={0}
+          activeOpacity={1}
           onPress={() => {
-            scrollYIndex.setValue(index)
-            // console.log('item clicked')
+            console.log('item clicked')
           }}>
           <Image style={styles.bgImage}
             source={{ uri: 'https://picsum.photos/500/300' }} />
@@ -111,127 +80,44 @@ const HomeScreen: React.FC = () => {
     );
   };
 
-  const swipeDown = (event: HandlerStateChangeEvent<any>) => {
-    if (event.nativeEvent.state === State.END) {
-      console.log('Down')
-      if (currentIndex < data.length) {
-        const newIndex: number = currentIndex + 1
-        console.info('wwwwwwwwwwwwwww', newIndex)
-        setCurrentIndex(newIndex)
-        scrollYIndex.setValue(newIndex)
-      }
-    }
-  }
-
-  const swipeUp = (event: HandlerStateChangeEvent<any>) => {
-    if (event.nativeEvent.state === State.END) {
-      console.log('UP')
-      if (currentIndex < data.length) {
-        const newIndex: number = currentIndex - 1
-        console.info('qqqqqqqqqqqqqqqqq', newIndex)
-        setCurrentIndex(newIndex)
-        scrollYIndex.setValue(newIndex)
-      }
-    }
-  }
-
-  const onGestureEvent = (event: GestureEvent<any>) => {
-    // console.log(event.nativeEvent)
-    const { translationY } = event.nativeEvent;
-    const divisor = 100.0;
-    let newIndex = currentIndex + (translationY / divisor);
-    if (newIndex < 0) newIndex = 0;
-    if (newIndex > data.length - 1) newIndex = data.length - 1;
-    console.log('newIndex',)
-
-    if (event.nativeEvent.translationY < 0) {
-      console.log("up")
-      if (currentIndex > 0) {
-        setCurrentIndex(Math.floor(currentIndex - newIndex))
-        scrollYIndex.setValue(Math.floor(currentIndex - newIndex))
-      }
-    } else {
-      console.log('down')
-      if (currentIndex < data.length - 1) {
-        if (currentIndex + newIndex < data.length) {
-          setCurrentIndex(Math.floor(currentIndex + newIndex))
-          scrollYIndex.setValue(Math.floor(currentIndex + newIndex))
-        }
-      }
-    }
-  }
-
-  const onHandlerStateChange = (event: GestureEvent<any>) => {
-    if (event.nativeEvent.oldState === State.BEGAN) {
-      console.log('Gesture started! Initial translationY:', event.nativeEvent.translationY);
-      if (!enableAnimation)
-        setEnableAnimation(true)
-    } else if (event.nativeEvent.oldState === State.ACTIVE && event.nativeEvent.state === State.END) {
-      console.log('Gesture ended! Final translationY:', event.nativeEvent.translationY);
-      if (enableAnimation)
-        setEnableAnimation(false)
-    }
-  }
-
 
   return (
     <View style={{
-      flex: 1
+      height: '100%',
+      width: '100%',
+      backgroundColor: 'yellow'
     }}>
-      <FlingGestureHandler
-        key={'down'}
-        direction={Directions.DOWN}
-        onHandlerStateChange={swipeDown}>
-        <FlingGestureHandler
-          key={'down'}
-          direction={Directions.UP}
-          onHandlerStateChange={swipeUp}>
-          {/* <PanGestureHandler onGestureEvent={onGestureEvent}
-      onHandlerStateChange={onHandlerStateChange}> */}
-          <FlatList
-            data={data}
-            keyExtractor={(_, index) => String(index)}
-            inverted
-            contentContainerStyle={styles.flatListContainer}
-            removeClippedSubviews={false}
-            scrollEnabled={false}
-            pagingEnabled
-            CellRendererComponent={({ item, index, children, style, ...props }) => {
-              const newStyle: StyleProp<ViewStyle> = [
-                style,
-                {
-                  zIndex: data.length - index,
-                }
-              ]
-              return (
-                <View key={index} style={newStyle}>
-                  {children}
-                </View>
-              )
-            }}
-            renderItem={renderItem}
-          />
-          {/* </PanGestureHandler> */}
-        </FlingGestureHandler>
-      </FlingGestureHandler>
-      <View style={{
-        flexDirection: 'row'
-      }}>
-        <View style={{ flex: 1 }}>
+      <Animated.FlatList
+        data={data}
+        keyExtractor={(_, index) => String(index)}
+        inverted
+        removeClippedSubviews={false}
+        snapToInterval={ITEM_HEIGHT}
+        decelerationRate={0}
+        bounces={false}
+        pagingEnabled
+        renderItem={renderItem}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        scrollEventThrottle={2}
+        CellRendererComponent={({ item, index, children, style, ...props }) => {
+          const newStyle: StyleProp<ViewStyle> = [
+            style,
+            {
+              zIndex: data.length - index,
+            }
+          ]
+          return (
+            <View key={index} style={newStyle}>
+              {children}
+            </View>
+          )
+        }}
+      />
 
-          <Button title="Up" onPress={() => {
-            setCurrentIndex(index => index + 1)
-            scrollYIndex.setValue(currentIndex)
-          }} />
-        </View>
-        <View style={{ flex: 1 }}>
 
-          <Button title="Down" onPress={() => {
-            setCurrentIndex(index => index + 1)
-            scrollYIndex.setValue(currentIndex)
-          }} />
-        </View>
-      </View>
     </View>
   );
 };
@@ -255,11 +141,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-  flatListContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: SPACING * 2
-  },
   cardContainer: {
     padding: 10,
     backgroundColor: `black`,
@@ -267,7 +148,6 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     borderColor: 'orange',
     height: ITEM_HEIGHT,
-    width: ITEM_WIDTH,
   },
   bgImage: {
     position: 'absolute',
